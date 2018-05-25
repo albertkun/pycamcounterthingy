65a772c4d19b0153cc23722a9f661eda8e8cac07
albertkun <albertk@gmx.com>
2018-05-24 18:55:56 -0700

    added function for the counter
    should work

--- pcT1v2.py
+++ pcT1v2.py
@@ -1,5 +1,4 @@
 #!usr/bin/python 
-
 import argparse
 import datetime
 import imutils
@@ -7,43 +6,39 @@ import math
 import cv2
 import numpy as np
 
-width = 1200
-height = 1200 
+#these are the pixel dimensions for the camera
+width = 800
+height = 800 
+
+#these are the extra off-sets to check how far away from entrance or exit lines
 offsetRefLines = 150
-minContour = 8000 #original was 12000
 
-textIn = 0
-textOut = 0
-
-# def testIntersectionIn(x, y):
-#     #157500  #changing it to 180000, makes it count In first (in the video)
-#     res = -450 * x + 400 * y + 157500
-#     #print ("this is res in {}").format(res)
-#     if((res >= -550) and  (res < 550)):
-#         print("printing x: ")
-#         print(str(x))
-#         print("printing y: ")
-#         print(str(y))
-#         print("printing res: )")
-#         print (str(res))
-#         return True
-#     return False
+#minContour is the minimum threshold that the camera will look for
+minContour = 8000 #original was 12000, smaller value means more objects will be picked up
 
 
+enterCounter = 0
+exitCounter = 0
 
-# def testIntersectionOut(x, y):
-#     #180000 #changing it to 157500 makes it count Out second (in the video)
-#     res = -450 * x + 400 * y + 180000  #this last number effects in and out (if both are same num they both in or both out)
-#     if ((res >= -550) and (res <= 550)):
-#         print("printing x: ")
-#         print(str(x))
-#         print("printing y: ")
-#         print(str(y))
-#         print("printing res: )")
-#         print (str(res))
-#         return True
+#function to check if an object has entered
+def checkIn(y, yEnterLine, yExitLine):
+    # how far is the object (y) from the "enter line's y-value"?
+    absoluteDistance = abs(y - yEnterLine)
+    # if it's less than or equal to 2 and less than the "exit line's y-value", then you've entered
+    if ((absoluteDistance <= 2) and (y < yExitLine)):
+        return 1
+    else:
+        return 0
 
-#     return False
+
+#function to check if an object has exited
+def checkOut(y, yEnterLine, yExitLine):
+    absoluteDistance = abs(y - yExitLine)
+    if ((absoluteDistance <= 2) and (y > yEnterLine)):
+        return 1
+    else:
+        return 0
+
 #camera = cv2.VideoCapture("test2.mp4") #testing using a video file 
 camera = cv2.VideoCapture(0)  #testing using a webcam feed 
 firstFrame = None
@@ -71,16 +66,31 @@ while True:
         continue
 
     # compute the absolute difference between the current frame and
-    # first frame
+    # initial frame
     frameDelta = cv2.absdiff(firstFrame, gray)
+
+    #check the framedelta feed
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
     # dilate the thresholded image to fill in holes, then find contours
     # on thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
+
+
+    cv2.imshow("Scanner", thresh)
     #cnts is the contours which helps detect what is in the frame --> gets passed in the following
     #for loop where the rectangle is then drawn
     _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # loop over the contours
+
+
+    yEnterLine = (height / 3) - offsetRefLines #Line for enter - horizontal 
+    yExitLine = (height / 3) + offsetRefLines #Line for exit - horizontal 
+
+
+
+    #these two lines only draw the line; does not do anything except to display
+    cv2.line(frame, (0, yEnterLine), (width, yEnterLine), (250, 0, 1), 2)
+    cv2.line(frame, (0, yExitLine), (width, yExitLine), (0, 0, 225), 2)
     for c in cnts:
         # if the contour is too small, ignore it
         if cv2.contourArea(c) < minContour:   #12000           
@@ -97,23 +107,24 @@ while True:
         # cv2.line(frame, (width / 2, 0), (width, 450), (250, 0, 1), 2) #blue line OUT
         # cv2.line(frame, (width / 2 - 50, 0), (width - 50, 450), (0, 0, 255), 2)#red line IN
 
-        yEnterLine = (height / 3) - offsetRefLines #Line for enter - horizontal 
-        yExitLine = (height / 3) + offsetRefLines #Line for exit - horizontal 
-
-        #these two lines only draw the line; does not do anything except to display
-        cv2.line(frame, (0, yEnterLine), (width, yEnterLine), (250, 0, 1), 2)
-        cv2.line(frame, (0, yExitLine), (width, yExitLine), (0, 0, 225), 2)
 
         #this is the X and Y center for each object rectangle 
         rectangleXCentroid = ((x + x + w) / 2)
         rectangleYCentroid = ((y + y + w) / 2)
         rectangleCentroid = (rectangleXCentroid, rectangleYCentroid)
 
-        #the red center of the rectangle  
-        print ("this is rectangleCenter x {}, y {}").format(x,y)
+        #the red center of the rectangle 
+        try:
+            print("this is rectangleCenter x {}, y {}").format(x,y)
+        except:
+            print "cant get center"
+
         cv2.circle(frame, rectangleCentroid, 1, (0, 0, 255), 5) 
 
-        if (checkIn(rectangleYCentroid, yEnterLine, yExitLine))
+        if (checkIn(rectangleYCentroid, yEnterLine, yExitLine)):
+            enterCounter += 1
+        if (checkOut(rectangleYCentroid, yEnterLine, yExitLine)):
+            exitCounter += 1
 
         # if(testIntersectionIn((x + x + w) / 2, (y + y + h) / 2)):
         #     textIn += 1
@@ -130,9 +141,9 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    cv2.putText(frame, "In: {}".format(str(textIn)), (10, 50),
+    cv2.putText(frame, "In: {}".format(str(enterCounter)), (10, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, "Out: {}".format(str(textOut)), (10, 70),
+    cv2.putText(frame, "Out: {}".format(str(exitCounter)), (10, 70),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
                 (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
